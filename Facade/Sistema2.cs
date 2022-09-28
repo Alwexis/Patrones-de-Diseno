6ec89502c6e7a8a81e45bc2a9bbc16f9028d036e
Ariel Silva <51482182+Alwexis@users.noreply.github.com>
2022-09-28 16:42:07 -0300

    Create Sistema2.cs

---
+++ Facade/Sistema2.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patrones_de_Diseno.clases {
+	public class Sistema2 {
+		public string operacion1() {
+			return "Operacion 1 lista!";
+		}
+		public string operacion2() {
+			return "Operacion 2 lista!";
+		}
+	}
+}
